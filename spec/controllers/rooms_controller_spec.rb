78535b7073765be6bca4b062018dad4202a67e4b
Manuel <manuelfloresv@gmail.com>
2018-05-04 00:10:45 +0200

    Add test for last_messages endpoint

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -16,4 +16,16 @@ RSpec.describe RoomsController, type: :controller do
     end
   end
 
+  describe "GET #last_messages" do
+    it "returns only last 20 messages" do
+      room = Room.create(name: "Test room 2")
+      30.times do
+        msg = Message.new(author: "Testman", text: "Test message")
+        room.messages.push(msg)
+      end
+      get :last_messages, :params => {:id => room.id}
+      json = JSON(response.body)
+      expect(json.length).to be_eql(20)
+    end
+  end
 end
